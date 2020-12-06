65a873549640fa2a0e329de9661d57658bed0e3c
Denilson V <deethedev@gmail.com>
2020-12-06 18:17:29 -0500

    Finished delete and update

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "CustomersControllers", type: :request do
+  #GET SHOW REQUEST
   describe "get customers_path" do
     it "renders the index view" do
       get customers_path
       expect(response.status).to eq(200)
     end
   end
+
+  #GET CUSTOMER (ID)
   describe "get customer_path" do
     it "renders the :show template" do
       customer = FactoryBot.create(:customer)
@@ -19,6 +22,7 @@ RSpec.describe "CustomersControllers", type: :request do
     end
   end
 
+  #GET NEW REQUEST
   describe "get new_customer_path" do
     it "renders the :new template" do
       customer = FactoryBot.create(:customer)
@@ -26,6 +30,8 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  #GET EDIT REQUEST
   describe "get edit_customer_path" do
     it "renders the :edit template" do
       customer = FactoryBot.create(:customer)
@@ -33,6 +39,8 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  #POST REQUEST VALID DATA
   describe "post customers_path with valid data" do
     it "saves a new entry and redirects to the show path for the entry" do
       customer_attributes = FactoryBot.attributes_for(:customer)
@@ -41,6 +49,8 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response).to redirect_to customer_path(id: Customer.last.id)
     end
   end
+
+  #POST REQUEST INVALID DATA
   describe "post customers_path with invalid data" do
     it "does not save a new entry or redirect" do
       customer_attributes = FactoryBot.attributes_for(:customer)
@@ -50,14 +60,22 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  #PUT REQUEST VALID DATA
   describe "put customer_path with valid data" do
     it "updates an entry and redirects to the show path for the customer" do
+      customer = FactoryBot.create(:customer)
       customer_attributes = FactoryBot.attributes_for(:customer)
-      put 'http://localhost/customers/update', params:  {"customer": {id: 1, customer: customer_attributes} }
-      # headers: authorized_user_headers
-      expect(response).to render_template(:show)
+      customer_attributes[:first_name] = 'New Name'
+      expect{ put customer_path( id: customer.id ), params:  { customer: customer_attributes }
+      }.to change{ customer.reload.first_name }
+
+      expect(response).to redirect_to(customer_path( id: customer.id ))
+      # redirect_to customer_path()
     end
   end
+
+  #PUT REQUEST INVALID DATA
   describe "put customer_path with invalid data" do
     it "does not update the customer record or redirect" do
       customer_attributes = FactoryBot.attributes_for(:customer)
@@ -67,12 +85,15 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response.status).to eq(302)
     end
   end
+  
+  #DELETE REQUEST
   describe "delete a customer record" do
     it "deletes a customer record" do
-      expect { delete :destroy, id: 1 }.to change(Customer, :count)
-      # expect { delete :destroy, params: {customer: customer_attributes}
-      # }.to change(Customer, :count)
-      expect(response.status).to eq(202)
+      customer = FactoryBot.create(:customer)
+      expect {delete customer_path( id: customer.id)  }.to change(Customer, :count)
+
+      expect(response).to redirect_to customers_path
+      # expect(response.status).to eq(200)
     end
   end
 end
