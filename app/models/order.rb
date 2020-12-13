c3e282070786f9996a8fc1a2027b6936fc9f3ab3
Denilson V <deethedev@gmail.com>
2020-12-13 12:18:06 -0500

    Complete Order scaffold with CRUD operations

---
+++ app/models/order.rb
@@ -0,0 +1,12 @@
+class Order < ApplicationRecord
+  belongs_to :customer
+
+  validates_presence_of :customer_id
+  validate :validate_course_id
+  validates :product_name, presence: true
+  validates :product_count, presence: true
+
+  def validate_course_id
+    errors.add(:customer_id, "is invalid") unless Customer.exists?(self.customer_id)
+  end
+end
