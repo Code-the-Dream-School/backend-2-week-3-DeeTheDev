033cbc2a7f27af9fc9d26123ccf9df49b95a03e1
Denilson V <deethedev@gmail.com>
2020-12-13 12:40:02 -0500

    Fixed mistype

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,11 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   validates_presence_of :customer_id
-  validate :validate_course_id
+  validate :validate_customer_id
   validates :product_name, presence: true
   validates :product_count, presence: true
 
-  def validate_course_id
+  def validate_customer_id
     errors.add(:customer_id, "is invalid") unless Customer.exists?(self.customer_id)
   end
 end
