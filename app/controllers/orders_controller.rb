e10bbeefc091f842151358d04009c93095701bf5
Denilson V <deethedev@gmail.com>
2020-12-13 15:25:57 -0500

    Fixed mistype

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,7 +59,7 @@ private
   def catch_not_found(e)
     Rails.logger.debug("We had a not found exception.")
     flash.alert = e.to_s
-    redirect_to customers_path
+    redirect_to orders_path
   end
 
 end
